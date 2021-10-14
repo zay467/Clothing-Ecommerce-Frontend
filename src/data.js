35b40362a7547216dd8d73b681e83310647a08f1
Zay Min Maw <zayminmaw77@gmail.com>
2021-10-14 14:08:54 +0630

    added pure react slider

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItem = [
+  {
+    id: 1,
+    img: "https://www.freepngimg.com/thumb/people/3-2-people-picture.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://www.freepngimg.com/thumb/dakota_johnson/33720-6-dakota-johnson-hd.png",
+    title: "WINTER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://www.freepngimg.com/thumb/dakota_johnson/33907-3-dakota-johnson-picture.png",
+    title: "POPULAR SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
