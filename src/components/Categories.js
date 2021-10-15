2f733979137007af9ad55498a7bfa1cfdea58800
Zay Min Maw <zayminmaw77@gmail.com>
2021-10-15 12:03:30 +0630

    added key for Category item

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -12,7 +12,7 @@ const Categories = () => {
   return (
     <Container>
       {categories.map((item) => (
-        <CategoryItem item={item} />
+        <CategoryItem key={item.id} item={item} />
       ))}
     </Container>
   );
